138148b6e57585460c2e12517d60a39350b845f4
xiuxin55 <906866703@qq.com>
2016-10-26 22:47:41 +0800

    薪资model

---
+++ BalanceReportModel/SalaryInfo.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BalanceModel
+{
+    /// <summary>
+    /// 薪资信息
+    /// </summary>
+    public class SalaryInfo : BaseModel
+    {
+        public string  ID { get; set; }
+        /// <summary>
+        /// 人员编码
+        /// </summary>
+        public string  StaffCode { get; set; }
+        /// <summary>
+        /// 人员姓名
+        /// </summary>
+        public string StaffName { get; set; }
+        /// <summary>
+        /// 岗位工资
+        /// </summary>
+        public string  JobSalary { get; set; }
+        /// <summary>
+        /// 专业技术职务津贴
+        /// </summary>
+        public string ProfessionAllowance { get; set; }
+        /// <summary>
+        /// 保留年功津贴
+        /// </summary>
+        public string  WebsiteManager { get; set; }
+        /// <summary>
+        /// 保留工资
+        /// </summary>
+        public string  ManagerTelphone { get; set; }
+        /// <summary>
+        /// 综合补贴
+        /// </summary>
+        public string  Institution { get; set; }
+        /// <summary>
+        /// 过渡期补贴
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 住房补贴
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 应发工资
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 住房公积金实缴额(个人合计)
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 养老保险实缴额(个人)
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 工会费
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 医疗保险实缴额(个人)
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 失业保险实缴额(个人)
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 大病医疗保险实缴额(个人)
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 年金
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 扣款自定义项1(抵税)
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 扣款合计
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 实发工资
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 应发绩效
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 奖励
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 考核
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 补扣上月
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 税收
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 风险金
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 实发绩效
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 实发合计
+        /// </summary>
+        public string Institution { get; set; }
+        /// <summary>
+        /// 备注说明
+        /// </summary>
+        public string Institution { get; set; }
+
+    }
+}
